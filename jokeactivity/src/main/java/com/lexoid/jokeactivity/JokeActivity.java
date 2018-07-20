186b88027cb0b2b5804b7f0738b58d1ae8cbbfea
Android Pro <offerscooperation@gmail.com>
2018-07-20 15:19:34 +0500

    init jokesLib added jokeactivity added

---
+++ jokeactivity/src/main/java/com/lexoid/jokeactivity/JokeActivity.java
@@ -0,0 +1,24 @@
+package com.lexoid.jokeactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    public static final String JOKE_EXTRA = "joke";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra(JOKE_EXTRA);
+
+        TextView jokeTv = findViewById(R.id.joke_tv);
+
+        jokeTv.setText(joke);
+    }
+}
---
+++ jokesLib/src/main/java/com/lexoid/jokeslib/JokesLib.java
@@ -0,0 +1,7 @@
+package com.lexoid.jokeslib;
+
+public class JokesLib {
+    public String getJoke(){
+        return "Nuck nuck! Who is there? Nuck-nucker";
+    }
+}
